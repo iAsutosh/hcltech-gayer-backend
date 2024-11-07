5250bd1dbb71a22145d2c1a3e00794616fbf5943
Lyrics kumar <liricskumar@colanonline.com>
2024-11-07 18:03:45 +0530

    TEST cases

--- src/routes/__test__/test.js
+++ src/routes/__test__/test.js
@@ -10,3 +10,21 @@ describe("GET HEALTH CHECK", () => {
        await supertest.get('/api/v1/healthcheck').expect(200);
     });
 });
+
+describe("POST LOGIN AUTH", () => {
+    it("should return 200", async () => {
+       await supertest.post('/api/v1/users/login').expect(200);
+    });
+});
+
+describe("POST REGISTER", () => {
+    it("should return 200", async () => {
+       await supertest.post('/api/v1/users/register').expect(200);
+    });
+});
+
+describe("POST DOCTORS SLOT", () => {
+    it("should return 200", async () => {
+       await supertest.post('/api/v1/users//doctors/slots').expect(200);
+    });
+});
