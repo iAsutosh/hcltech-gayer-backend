924017fe6c4fd5430af8a52f1b58a1c717fa0caf
iAsutosh <asutosh4077@gmail.com>
2024-11-07 15:18:13 +0530

    doc model added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -12,9 +12,7 @@ const router = Router()
 router.route("/register").post(
     registerUser
     )
-
 router.route("/login").post(loginUser)
-
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
 
