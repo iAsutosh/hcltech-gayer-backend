a0db4686bde13304aca48f8b19f173dd84896b64
iAsutosh <asutosh4077@gmail.com>
2024-11-07 16:02:15 +0530

    fix: bug doc

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -16,7 +16,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
-router.route("/doctorinfo").post(verifyJWT,  getDoctorsBySpecialization)
+router.route("/doctors/slots").get(verifyJWT,  getDoctorsBySpecialization)
 
 
 export default router
